3aa31a17db24e24f38f6fafd20784a6543acd2ce
sivasaran558 <sivasaran558@gmail.com>
2017-03-31 12:49:41 +0530

    Maven Project

---
+++ src/main/java/ReportingAPIAutomation/ReportingAPIAutomation/App.java
@@ -0,0 +1,13 @@
+package ReportingAPIAutomation.ReportingAPIAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
